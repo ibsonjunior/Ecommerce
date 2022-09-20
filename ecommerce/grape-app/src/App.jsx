f4df30d820d431d403937785bbd734f57b1d8da1
ibsonjunior <ibsonnjunior@gmail.com>
2022-09-20 12:32:54 -0300

    atualização bootstrap

--- ecommerce/grape-app/src/App.jsx
+++ ecommerce/grape-app/src/App.jsx
@@ -15,36 +15,10 @@ function App() {
 
   return (
     <>
-      <div className="container">
+    <Navbar statusSidebar={statusSidebar} setSidebar={setSidebar} />
 
-        <div className="row">
-          <div className="col">
-            {
-              /*
-                O componente Nav é responsável pela navegação
-                da Aplicação.
-              */
-            }
-            <Navbar statusSidebar={statusSidebar} setSidebar={setSidebar} />
-
-            <Sidebar statusSidebar={statusSidebar} setSidebar={setSidebar}/>
-          </div>
-        </div>
-
-        <div className="row my-3">
-				  <div className="col">
-					  {
-						  <Routes>
-							  <Route
-								  path="/"
-								  element={<Home />}
-							  />
-						  </Routes>
-					  }
-				  </div>
-			  </div>
-		  </div>
-     
+    <Sidebar statusSidebar={statusSidebar} setSidebar={setSidebar}/>
+      <Home/>
       <WineProduct />
 
       <Wines/>
