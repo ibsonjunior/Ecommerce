d4c2213e874edea609b75cd7ded47328a40471b3
ibsonjunior <ibsonnjunior@gmail.com>
2022-09-29 00:47:08 -0300

    Att pag wines

--- ecommerce/grape-app/src/pages/Wines.jsx
+++ ecommerce/grape-app/src/pages/Wines.jsx
@@ -1,114 +1,165 @@
-import "react-responsive-carousel/lib/styles/carousel.min.css";
-import Cordeiro from '../assets/Vinho_Branco/Cordeiro.png'
-import GuardaRios from '../assets/Vinho_Branco/GuardaRios.png'
+
+import Slider from "react-slick";
 import Footer from '../components/Footer'
-import React, { useState, useEffect } from 'react'
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+
 
 export default function Wines() {
+  var settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    initialSlide: 0,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initialSlide: 2,
+          infinite: true
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true
+        }
+      }
+    ]
+  };
 
-  const [wineList, setWineList] = useState([])
+  const [wines, setWines] = useState([]);
 
-  useEffect(() => {
-    fetch("http://54.177.105.189:9000/products").then((response) => {
-      response.json().then((data) => {
-        setWineList(data);
-        console.log(data);
-      });
-    });
-  }, []);
+    const url = "http://54.177.105.189:8081/products"
+    useEffect(() => {
+        fetch(url)
+            .then(responseWines => responseWines.json())
+            .then(WinesJSON => setWines(WinesJSON))
 
-  let vinhoTinto = [];
-  let vinhoBranco= [];
-  let vinhoVerde= [];
+          
+    }, []);
 
-  vinhoTinto = wineList.filter((vinho) => {
-    if(vinho.categories[0].id === 1 ){
-      return vinho;
-    }
-  })
+    
 
-  vinhoBranco = wineList.filter((vinho) => {
-    if(vinho.categories[0].id === 2 ){
-      return vinho;
-    }
-  })
-
-  vinhoVerde = wineList.filter((vinho) => {
-    if(vinho.categories[0].id === 3 ){
-      return vinho;
-    }
-  })
-
-  console.log(vinhoTinto)
-  console.log(vinhoBranco)
-  console.log(vinhoVerde)
 
   return(
 
     <>
+  
         <div className="container_wines">
           <div className="wines">
          
-            <div>
+            <div className="div_wine_titles">
               <h1 className="wine_titles">Vinhos</h1>
             </div>
 
-            <h2 className="wine_name"> vinhoTinto.categories </h2>
-             
-            <section className="wines_section"> 
-              <div
-                id="carouselExampleFade"
-                className="carousel slide carousel-fade"
-                data-bs-ride="carousel"
-              >
-                <div className="carousel-inner carousel-div ">
-                  <div className="carousel-item carousel-wine-item active">
-                    <img src={Cordeiro} className="d-block w-100" alt="..." />
-                  </div>
-                  <div className="carousel-item carousel-wine-item">
-                    <img src={GuardaRios} className="d-block w-100" alt="..." />
-                  </div>
-                  <div className="carousel-item carousel-wine-item">
-                    <img src={Cordeiro} className="d-block w-100" alt="..." />
-                  </div>
-                  <div className="carousel-item carousel-wine-item">
-                    <img src={GuardaRios} className="d-block w-100" alt="..." />
-                  </div>
-                  <div className="carousel-item carousel-wine-item">
-                    <img src={Cordeiro} className="d-block w-100" alt="..." />
-                  </div>
-                </div>
-
-                <button
-                  className="carousel-control-prev"
-                  type="button"
-                  data-bs-target="#carouselExampleFade"
-                  data-bs-slide="prev"
-                >
-                  <span
-                    className="carousel-control-prev-icon"
-                    aria-hidden="true"
-                  ></span>
-                  <span className="visually-hidden">Previous</span>
-                </button>
-                <button
-                  className="carousel-control-next"
-                  type="button"
-                  data-bs-target="#carouselExampleFade"
-                  data-bs-slide="next"
-                >
-                  <span
-                    className="carousel-control-next-icon"
-                    aria-hidden="true"
-                  ></span>
-                  <span className="visually-hidden">Next</span>
-                </button>
-              </div>
-
+            <section className="wines_section">
+              <h2 className="wine_name">Vinho Tinto</h2>
+              <div>
+              <Slider {...settings}>
+                {wines.filter((wines) => wines.categories.find(categorie => categorie.id === 1)).map((wine) =>{
+                return(  
+                  <Link className="" to="/Carrinho">
+                    <div key={wine.id}>
+                          <div className="carousel-wine-item ">
+                            <img className='img_wine' src={wine.image} alt="wineImage"/>
+                            <div className="div_wine">
+                                <h4 className='title_'>{wine.title}</h4>
+                                <p className="description_wine">{wine.description}</p>
+                                </div>
+                          </div>
+                      
+                      
+                        </div>
+                  </Link>                        
+                      
+                 
+                    )
+                  })}
+                </Slider>
+      
+                 </div> 
+                
             </section>
 
+          
+            <section className="wines_section">
+              <h2 className="wine_name">Vinho Branco</h2>
+              <div>
+              <Slider {...settings}>
+                {wines.filter((wines) => wines.categories.find(categorie => categorie.id === 2)).map((wine) =>{
+                return(  
+                  <Link className="" to="/Carrinho">
+                    <div key={wine.id}>
+                          <div className="carousel-wine-item ">
+                            <img className='img_wine' src={wine.image} alt="wineImage"/>
+                            <div className="div_wine">
+                                <h4 className='title_'>{wine.title}</h4>
+                                <p className="description_wine">{wine.description}</p>
+                                </div>
+                          </div>
+                      
+                      
+                        </div>
+                  </Link>                        
+                      
                  
-            <Footer />
+                    )
+                  })}
+                </Slider>
+      
+                 </div> 
+                
+            </section>
+
+            <section className="wines_section">
+              <h2 className="wine_name">Vinho Verde</h2>
+              <div>
+              <Slider {...settings}>
+                {wines.filter((wines) => wines.categories.find(categorie => categorie.id === 3)).map((wine) =>{
+                return(  
+                  <Link className="" to="/Carrinho">
+                    <div key={wine.id}>
+                          <div className="carousel-wine-item ">
+                            <img className='img_wine' src={wine.image} alt="wineImage"/>
+                            <div className="div_wine">
+                                <h4 className='title_'>{wine.title}</h4>
+                                <p className="description_wine">{wine.description}</p>
+                                </div>
+                          </div>
+                      
+                      
+                        </div>
+                  </Link>                        
+                      
+                 
+                    )
+                  })}
+                </Slider>
+      
+                 </div> 
+                
+            </section>
+
+
+            
+           
 
           </div>
 
